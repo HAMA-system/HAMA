03ace3300de16452a300d74e153e82d48d281862
110w110 <110w110.official@gmail.com>
2022-03-16 11:37:30 +0900

    first commit

--- main.py
+++ main.py
@@ -17,5 +17,14 @@ if __name__ == '__main__':
     # set_chromedriver()
     file = xlsxFileController.load_xls('example.xlsx')
     data = xlsxFileController.get_cell_data(file, 'Sheet1', 'B4')
+
+    # firstcell = 'B4'
+    # lastcell = 'F4'
+    #
+    # print(firstcell[1:])
+    # print(chr(ord(lastcell[:1])+1)+firstcell[1:])
     print(data)
+
+    data2 = xlsxFileController.get_singleline_data(file,'Sheet1','B4','G4')
+    print(data2)
     print('h')
--- xlsxFileController.py
+++ xlsxFileController.py
@@ -11,4 +11,15 @@ def get_cell_data(filename, sheetname ,cell):
     sheet = filename.get_sheet_by_name(sheetname)
     return sheet[cell].value
 
-# def get_singleline_data(file, sheet, cell):
+def get_singleline_data(filename, sheetname, firstcell, lastcell):
+    cell = firstcell
+    data = []
+
+    if firstcell[1:]!=lastcell[1:]:
+        print("첫 셀과 마지막 셀이 같은 행에 있지 않음")
+        return data
+
+    while cell!=lastcell:
+        data.append(get_cell_data(filename,sheetname,cell))
+        cell = chr(ord(cell[:1])+1) + cell[1:]
+    return data
