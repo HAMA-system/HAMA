4929fad6025c7dd390feeb4d7cc38f51fa633f61
110w110 <110w110.official@gmail.com>
2022-03-17 09:15:56 +0900

    코드 정리 22.3.17 09:15

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 import xlsxFileController
 import errorController
@@ -9,22 +8,15 @@ def set_chromedriver():
         driver = webdriver.Chrome("./chromedriver")
     except:
         errorController.errorMsg(0)
-    url = 'https://ngw.hongik.ac.kr/myoffice/ezportal/index_portal.aspx'
     return driver
 
 if __name__ == '__main__':
     driver = set_chromedriver()
-    driver.get('https://ngw.hongik.ac.kr/myoffice/ezportal/index_portal.aspx')
-
+    driver.get('https://itss.hongik.ac.kr/GateWeb/index.aspx#')
     
     file = xlsxFileController.load_xls('example.xlsx')
     data = xlsxFileController.get_cell_data(file, 'Sheet1', 'B4')
 
-    # firstcell = 'B4'
-    # lastcell = 'F4'
-    #
-    # print(firstcell[1:])
-    # print(chr(ord(lastcell[:1])+1)+firstcell[1:])
     print(data)
 
     data2 = xlsxFileController.get_singleline_data(file,'Sheet1','B4','G4')
