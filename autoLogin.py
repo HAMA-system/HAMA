6ddee70629adbc8f3cb673e26cea126e7059e8e7
pxxnxx <kmin9811@naver.com>
2022-03-17 10:58:48 +0900

    자동로그인 추가(예외케이스 처리 필요)

---
+++ autoLogin.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def fname(driver,name,value):
+    driver.find_element_by_name(name).send_keys(value)
+def cname(driver,name):
+    driver.find_element_by_name(name).click()
+def ename(driver,name):
+    driver.find_element_by_name(name).send_keys(Keys.ENTER)
+def cpath(driver,name):
+    driver.find_element_by_path(name).click()
+
+def login(driver):
+
+    fname(driver,'USER_ID','jpjung')
+    fname(driver,'PASSWD','jinsu781!!')
+    ename(driver,'PASSWD')
+    ename(driver,'PASSWD')
+
+    # time.sleep(1)
+    # alert = driver.switch_to_alert
+    # alert.dismiss()
+    # 비밀번호 변경 필요 알림 제거용
+
+
+    # if EC.alert_is_present():
+    #     alert = driver.switch_to_alert
+    #     alert.dismiss()
+    #
+    # try:
+    #     WebDriverWait(webbrowser, 3).until(EC.alert_is_present())
+    #     alert = driver.switch_to_alert
+    #     alert.dismiss()
+    # except:
+    #     time.sleep(1)
+
+
+
+    return driver
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get("https://www.hongik.ac.kr/login.do?Refer=https://ngw.hongik.ac.kr/login_hongik.aspx")
+    login(driver)
