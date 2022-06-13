eab477d56455df51e36923dcb9e4947766ec65ac
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-06-13 15:16:05 +0900

    timeError check 초기 버전

--- RUN_DRAFT.py
+++ RUN_DRAFT.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 from linkData import *
@@ -5,11 +6,36 @@ from autoLogin import *
 from alertController import *
 from selenium.webdriver import ActionChains
 
+t = 0
+def timeError():
+    global d
+    global t
+    tt = 0
+    while tt < 20:
+        tt = t
+        # print(tt)
+        tt += 1
+        t = tt
+        time.sleep(1)
+    try:
+        d[1].close()
+    except:
+        pass
+    d.get("https://ngw.hongik.ac.kr/myoffice/ezportal/index_portal.aspx")
+    print("시간 초과로 재실행합니다.")
+    t = 0
+    draft(d)
+
 def expand_shadow_element(driver, element):
     shadow_root = driver.execute_script('return arguments[0].shadowRoot', element)
     return shadow_root
-
+d = None
 def draft(driver):
+    global d
+    global t
+    d = driver
+    te = threading.Thread(target=timeError)
+    te.start()
     driver.switch_to.frame(기안_프레임1)
     cpath(driver, 결재문서)
     time.sleep(3)
@@ -19,6 +45,7 @@ def draft(driver):
     time.sleep(1)
     while True:
         while True:
+            te.t = 0
             table = driver.find_element_by_id('DocList')
             print()
             i = 1
@@ -31,12 +58,14 @@ def draft(driver):
             print("\n인쇄를 할 문서들의 시작번호와 끝번호를 입력해주세요. ex) 1 20")
             print("혹은 이동할 페이지 번호를 입력해주세요. ex) 2")
             print("검색 키워드가 있는 경우 입력해주세요.")
+            t = -10000
             inp = input()
             try:
                 inp = list(map(int,inp.split()))
                 start, end = inp[0], inp[1]
                 break
             except:
+                t = 10
                 try:
                     inp = int(inp[0])
                     page = 페이지_변경[:29] + str(inp + 2) + 페이지_변경[30:]
@@ -56,14 +85,13 @@ def draft(driver):
         #     cpath(driver, page)
         #     time.sleep(1)
         #     continue
-
         for i in range(start, end+1):
             # 시작할 때, 반복할 때 주소가 달라야 클릭이 됨
+            t = 5
             if i == start:
                 num = 문서번호1[:64] + str(i) + 문서번호1[65:]
             else:
                 num = 문서번호2[:67] + str(i) + 문서번호2[68:]
-
             print(i,"번 문서 인쇄중...",sep='')
             cpath(driver, num)
             time.sleep(3)
@@ -71,13 +99,15 @@ def draft(driver):
             time.sleep(1)
             driver.switch_to.frame(인쇄_프레임)
             cpath(driver, 문서인쇄)
+            t = 0
             while True:
                 try:
                     driver.switch_to.window(driver.window_handles[1])
                     break
                 except:
                     time.sleep(2)
-
+            t = 5
+            d = driver
             # time.sleep(10)
 
             # ** shadow-root 같은 element는 find_element(by=) 이용해야함 **
